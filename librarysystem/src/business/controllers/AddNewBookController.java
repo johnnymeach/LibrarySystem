b27622eecf5f9e05bd11c39c8f9a670735c114d6
johnnymeach <johnny.meach@pathmazing.com>
2016-01-21 13:38:27 -0600

    Add Book and Author

---
+++ librarysystem/src/business/controllers/AddNewBookController.java
@@ -0,0 +1,50 @@
+package business.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import business.util.Helper;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class AddNewBookController implements Initializable{
+	
+	Stage stage;
+	Helper helper = new Helper();
+	@FXML
+	private Label lblClose;
+	@FXML
+	private Button btn_NewAuthor;
+	
+	public void addAuthor(ActionEvent event) throws Exception{
+		String AuthorView = "../views/author/addAuthor.fxml";
+		String viewTitle = "Add New Author";
+		helper.loadNewStage(stage,lblClose, AuthorView, viewTitle, false);
+		System.out.println("xyz");
+		
+	}
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+		Platform.runLater(() -> {
+			lblClose.setOnMouseClicked((MouseEvent event) -> {
+				Platform.exit();
+				System.exit(0);
+			});
+		});
+
+	}
+	
+	public void test(){
+		System.out.println("hello");
+	}
+
+	
+
+}
